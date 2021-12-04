5a40d1a482f68115f8ba52268e81580c6640e7c5
Yanni Pang <yanni@bu.edu>
2021-12-04 13:18:39 -0500

    removed learn react link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Code:</h2>
+                <textarea value={this.state.value} onChange={this.handleChange} rows="25" cols="100"/>
+                <h2>JSONified:</h2>
+                <textarea value={JSON.stringify(this.state.value) || ""} rows="25" cols="100"/>
+
+            </div>
+        );
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <script>{`var textareas = document.getElementsByTagName('textarea');
+            textareas.onkeydown = function(e){
+              if(e.keyCode==9 || e.which==9){
+                e.preventDefault();
+                var s = this.selectionStart;
+                this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+                this.selectionEnd = s+1;
+              }
+            }
+          }`}</script>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <NameForm/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
